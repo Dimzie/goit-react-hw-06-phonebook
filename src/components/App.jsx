1e1507d1ef014df5fdb2ab4d6ea0eac3615eed00
Dmytro Yankovskiy <dimayanik97@gmail.com>
2023-10-31 11:55:04 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,11 @@ import Phonebook from 'components/Phonebook/Phonebook';
 import ContactsList from './ContactsList/ContactsList';
 import { useSelector, useDispatch } from 'react-redux';
 import { filterInput } from 'redux/filterSlice';
-import { getfilteredContacts } from 'redux/selectors';
+import { getFilter, getfilteredContacts } from 'redux/selectors';
 
 export function App() {
   const filteredContacts = useSelector(getfilteredContacts);
+  const filter = useSelector(getFilter);
   const dispatch = useDispatch();
   const filterChange = e => {
     dispatch(filterInput(e.target.value));
@@ -14,14 +15,16 @@ export function App() {
   return (
     <>
       <Phonebook filteredContacts={filteredContacts} />
-      {filteredContacts.length === 0 ? (
+
+      <h2>Search for contact:</h2>
+      <input onChange={filterChange} type="text" name="filter" />
+
+      {filteredContacts.length === 0 && filter.length === 0 ? (
         <p>Your contact book is empty!</p>
+      ) : filteredContacts.length === 0 && filter.length !== 0 ? (
+        <p>No contacts with name : {filter}</p>
       ) : (
-        <>
-          <h2>Search for contact:</h2>
-          <input onChange={filterChange} type="text" name="filter" />
-          <ContactsList filteredContacts={filteredContacts} />
-        </>
+        <ContactsList filteredContacts={filteredContacts} />
       )}
     </>
   );
